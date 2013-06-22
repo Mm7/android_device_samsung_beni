73c835b5133baad99acd663d0569b2bd309807e3
erikcas <erikcas1972@gmail.com>
2013-06-22 16:14:40 +0200

    aokp

--- aokp.mk
+++ aokp.mk
@@ -4,11 +4,11 @@ TARGET_BOOTANIMATION_NAME := vertical-240x320
 # Inherit device configuration for Beni
 $(call inherit-product, device/samsung/beni/full_beni.mk)
 
-# Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+# Inherit some common aokp stuff.
+$(call inherit-product, vendor/aokp/configs/common_phone.mk)
 
 # Include GSM stuff
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/cm/configs/gsm.mk)
 
 
 # Release name and versioning
@@ -17,4 +17,4 @@ PRODUCT_VERSION_DEVICE_SPECIFIC := -GT-S5670
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := beni
-PRODUCT_NAME := cm_beni
+PRODUCT_NAME := aokp_beni
